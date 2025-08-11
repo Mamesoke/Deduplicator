86141568234ae3034c169d3b151ff26d41fe16a3
Kevin M. <32903290+Mamesoke@users.noreply.github.com>
2025-08-11 18:00:48 +0200

    Use worker pool to hash files concurrently

--- deduplicator/walker.go
+++ deduplicator/walker.go
@@ -5,31 +5,59 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"runtime"
+	"sync"
 )
 
 func WalkAndHash(root string, hashFunc func(string) (string, error)) ([]FileInfo, error) {
-	var files []FileInfo
+	var (
+		files   []FileInfo
+		paths   = make(chan string)
+		results = make(chan FileInfo)
+		wg      sync.WaitGroup
+	)
+
+	workerCount := runtime.NumCPU()
+	wg.Add(workerCount)
+	for i := 0; i < workerCount; i++ {
+		go func() {
+			defer wg.Done()
+			for path := range paths {
+				info, err := os.Stat(path)
+				if err != nil {
+					continue
+				}
+				hash, err := hashFunc(path)
+				if err != nil {
+					continue
+				}
+				results <- FileInfo{
+					Path:         path,
+					Size:         info.Size(),
+					Hash:         hash,
+					LastModified: info.ModTime().Unix(),
+				}
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
 
 	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil || d.IsDir() {
-			return nil // ignorar errores o carpetas
+			return nil
 		}
-
-		info, _ := os.Stat(path)
-		hash, err := hashFunc(path)
-		if err != nil {
-			return nil // opcional: loggear errores por archivo
-		}
-
-		files = append(files, FileInfo{
-			Path:         path,
-			Size:         info.Size(),
-			Hash:         hash,
-			LastModified: info.ModTime().Unix(),
-		})
-
+		paths <- path
 		return nil
 	})
+	close(paths)
+
+	for fi := range results {
+		files = append(files, fi)
+	}
 
 	return files, err
 }
